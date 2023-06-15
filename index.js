956dd84aee9aaaa7f27616051ac40239b8f6eb5d
Ender Romero <endjr1976@gmail.com>
2023-06-15 04:20:13 -0300

    90 percent done

--- index.js
+++ index.js
@@ -1,66 +1,100 @@
 // importar los elementos
 
 const pokemonId = document.querySelector("#pokeId");
-const searchButton = document.querySelector("#searchButton");
+const form = document.querySelector("form");
 const PokemonCard = document.querySelector("#PokemonCard");
+const searchMsg = document.querySelector(".search-msg")
 
 // Direccion url
 
 const pokeUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-// funcion para el llamado de la api para retornar la data necesaria
+// Funcion para obtener la data del pokemon consultado
 
-const pokeFetch = async (url) => {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+const requestPokemon = async (pokemon) => {
+    const response = await fetch(pokeUrl + pokemon);
+    const data = await response.json();
+    return data;    
 };
+
 
 // Funcion para extraer la data necesaria del pokemon
 
-const pokemonTemplate = (pokemon) => {
+const getPokemonData = (pokemonData) => {
   return {
-    image: pokemon.sprites.other.home.front_default,
-    name: pokemon.name.toUpperCase(),
-    height: pokemon.height / 10,
-    weight: pokemon.weight / 10,
-    types: pokemon.types,
+    image: pokemonData.sprites.other.home.front_default,
+    name: pokemonData.name.toUpperCase(),
+    height: pokemonData.height / 10,
+    weight: pokemonData.weight / 10,
+    types: pokemonData.types,
   };
 };
 
-// Funcion para desglosar los tipos en la card del Pokemon
 
-const createTypeCards = (types) => {
-  return types
-    .map((tipo) => {
-      return `<span class="type">${tipo.type.name}</span>`;
-    })
-    .join("");
-};
+// Funcion para crear el template del Pokemon buscado
 
-// Funcion para crear la card del Pokemon
+const createPokemonCard = (pokemonData) => {
+    const { image, name, height, weight, types } = getPokemonData(pokemonData);
+    return `
+      <div class="pokem">
+          <img class="poke-img" src="${image}" alt="${image}-image">
+          <h2>${name}</h2>
+          
+          <p class="height">Altura: ${height} m</p>
+          <p class="weight">Peso: ${weight} Kg</p>
+      </div>
+    `;
+  };
+  
 
-const createPokemonCard = (pokemon) => {
-  const { image, name, height, weight, types } = pokemonTemplate(pokemon);
-  return `
-    <div class="pokem">
-        <img src="${image}" alt="${image}-image">
-        <h2>${name}</h2>
-        <div class="pokeTypes">
-            ${createTypeCards(types)}
-        </div>
-        <p class="height">Altura: ${height} m</p>
-        <p class="weight">Peso: ${weight} Kg</p>
-    </div>
-  `;
-};
+// Funcion para renderizar la card en el DOM
 
-// Funcion para renderizar la pokemon Card
-
-const renderPokemon = (pokemon) => {
-    PokemonCard.innerHTML = createPokemonCard(pokemon)
+const renderPokemonCard = (pokemonData) => {
+    PokemonCard.innerHTML = createPokemonCard(pokemonData)
 }
 
-searchButton.addEventListener("click", (e) => {
-  console.log(pokeFetch(pokeUrl + pokemonId.value));
-});
+
+// Funcion para validar que el input este vacio
+
+const isEmptyInput = () => {
+    return pokemonId.value.trim() === "";
+}
+
+
+// Funcion para validar si el id del Pokemon existe
+
+const isInvalidPokemonId = (pokemonData) => {
+    return !pokemonData.id;
+}
+
+
+// Funcion de busqueda y renderizado de la card en el Dom
+
+const searchPokemon = async (e) => {
+    e.preventDefault();
+
+    if(isEmptyInput()) {
+        alert("Debes ingresar el Id del Pokemon a Buscar")
+        return;
+    }
+
+    const pokemon = pokemonId.value.trim();
+    const fetchedPokemon = await requestPokemon(pokemon);
+
+    if(isInvalidPokemonId(fetchedPokemon)) {
+        alert("El Id ingresado no corresponde a ningun Pokemon registrado");
+        form.reset();
+        return
+    }
+
+    renderPokemonCard(fetchedPokemon)
+}
+
+const init = () => {
+    form.addEventListener("submit", searchPokemon)
+}
+
+init();
+
+
+
